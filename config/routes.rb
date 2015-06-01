e0a5b6c6d960584fbfe29f2d2545e38609f16375
Ebenezer Sefah <esefah@nextjump.com>
2015-05-31 23:46:45 -0400

    home controller and dashboard page-default

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Studied::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#dashboard'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
